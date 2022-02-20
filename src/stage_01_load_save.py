19c3c2d0309c32c52163bbbb44422ba4de286f91
John A H <ahjohn98@gmail.com>
2022-02-21 04:56:16 +0530

    stage 1 added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -2,15 +2,15 @@ from src.utils.all_utils import read_yaml, create_directory
 import argparse
 import pandas as pd
 import os
-import shutil
-from tqdm import tqdm
-import logging
+import shutil#for copy operation
+from tqdm import tqdm # to get the progress bar of the copying
+# import logging
 
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
-log_dir = "logs"
-os.makedirs(log_dir, exist_ok=True)
-logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
-                    filemode="a")
+# logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+# log_dir = "logs"
+# os.makedirs(log_dir, exist_ok=True)
+# logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+#                     filemode="a")
 
 def copy_file(source_download_dir, local_data_dir):
     list_of_files = os.listdir(source_download_dir)
@@ -27,8 +27,8 @@ def get_data(config_path):
     local_data_dirs = config["local_data_dirs"]
 
     for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=2, desc= "list of folders", colour="red"):
-        create_directory([local_data_dir])
-        copy_file(source_download_dir, local_data_dir)
+        create_directory([local_data_dir]) #function defined in the all utils
+        copy_file(source_download_dir, local_data_dir) # function defined above
 
 
 if __name__ == '__main__':
@@ -38,10 +38,10 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-    try:
-        logging.info(">>>>> stage one started")
-        get_data(config_path=parsed_args.config)
-        logging.info("stage one completed! all the data are saved in local >>>>>\n")
-    except Exception as e:
-        logging.exception(e)
-        raise e
+    # try:
+    #     logging.info(">>>>> stage one started")
+    #     get_data(config_path=parsed_args.config)
+    #     logging.info("stage one completed! all the data are saved in local >>>>>\n")
+    # except Exception as e:
+    #     logging.exception(e)
+    #     raise e
